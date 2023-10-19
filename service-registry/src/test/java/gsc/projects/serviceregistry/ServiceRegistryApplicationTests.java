60674a8054842cfe0d29d522fa8da2c8776edde3
Gonçalo <goncaloscravo@gmail.com>
2023-10-19 12:35:05 +0100

    Add: Spring Cloud Netflix Eureka Server for service registry

---
+++ service-registry/src/test/java/gsc/projects/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package gsc.projects.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
